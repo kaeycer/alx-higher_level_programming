600402462b828b794a99ae1c8e8e8545af515659
Mpumelelo Mayisela <73696205+kaeycer@users.noreply.github.com>
2024-03-19 05:47:30 +0200

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,8 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-
-    counter = 0
-    i = len(my_list)
-    while counter < i:
-        old_list[counter] = my_list[counter]
-        counter += 1
+    old_list = my_list[:]
     if (idx < 0) or (idx > len(my_list) - 1):
         return (my_list)
     else:
-        # el = my_list
         old_list[idx] = element
         return (old_list)
