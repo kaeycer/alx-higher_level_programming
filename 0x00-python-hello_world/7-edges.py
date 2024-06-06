ce0de5061a887b44e4fa7b4248f2231e502b9b1f
Mpumelelo Mayisela <73696205+kaeycer@users.noreply.github.com>
2024-06-06 07:49:16 +0200

    Update 7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -4,5 +4,4 @@ word_first_3 = word[:3]
 print(f"First 3 letters: {word_first_3}")
 word_last_2 = word[-2:]
 print(f"Last 2 letters: {word_last_2}")
-middle_word = word[1:-1]
-print(f"Middle word: {middle_word}")
+print(f"Middle word: {word[1:-1]}")
