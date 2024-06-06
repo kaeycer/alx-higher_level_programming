35199dcc150b576bc25a8f3b93189de7bb2a4f56
Mpumelelo Mayisela <73696205+kaeycer@users.noreply.github.com>
2024-06-06 07:50:15 +0200

    Update 7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:3]
-print(f"First 3 letters: {word_first_3}")
-word_last_2 = word[-2:]
-print(f"Last 2 letters: {word_last_2}")
+
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[-2:]}")
 print(f"Middle word: {word[1:-1]}")
