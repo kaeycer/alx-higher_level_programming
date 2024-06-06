76a85a882400d711de3f874b5dba38f367648d93
Mpumelelo Mayisela <73696205+kaeycer@users.noreply.github.com>
2024-06-06 08:09:42 +0200

    Update 6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-print("Welcome to {}".format(str1), end=' ')
-print("{}!".format(str2))
+print(f"Welcome to {str1}", end=' ')
+print(f"{str2}!")
